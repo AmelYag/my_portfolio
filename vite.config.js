276d4e10b6e45de13536c9f1641f2c4dbc16912f
AmelYag <amelyagoub2005@gmail.com>
2025-07-06 21:50:18 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my_portfolio/',
-  plugins: [react()]
+  plugins: [react()],
+  base: "/my_portfolio"  // 👈 Important for GitHub Pages
 })
-
-
-
