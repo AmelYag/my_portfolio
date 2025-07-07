31bab133504f9b558d946c397b8f15fb648a39ac
AmelYag <amelyagoub2005@gmail.com>
2025-07-07 11:49:59 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my_portfolio"  // 👈 Important for GitHub Pages
+  base: './',  // 👈 Important for GitHub Pages
 })
